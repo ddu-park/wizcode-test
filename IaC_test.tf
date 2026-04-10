e55ffbc7cc1055a4f4727cd51a80e9890ea44b30
sapark <tndk3109@gmail.com>
2026-04-10 16:51:41 +0900

    update

--- IaC_test.tf
+++ IaC_test.tf
@@ -2,3 +2,15 @@ resource "aws_s3_bucket" "test" {
   bucket = "wiz-test-public"
   acl    = "public-read"
 }
+
+resource "aws_s3_bucket" "test2" {
+  bucket = "wiz-test-public"
+  acl    = "public-read"
+}
+
+# wiz-scan ignore-block
+resource "google_storage_bucket" "example" {
+  name          = "image-store.com"
+  location      = "EU"
+  force_destroy = true
+}
